ab0064a8331e62b310a3250723972b32ee4c69f7
Sjors van Holst <sjorsvanholst@gmail.com>
2022-06-12 17:44:46 +0200

    ✨ Added basic multiplayer

--- Assets/OtherController.cs
+++ Assets/OtherController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 using NativeWebSocket;
 
-public class Location
+public class Patch
 {
   public float x;
   public float y;
+  public string uuid;
 }
 
 public class OtherController : MonoBehaviour
@@ -25,7 +26,7 @@ public class OtherController : MonoBehaviour
     try
     {
       #if !UNITY_WEBGL || UNITY_EDITOR
-        websocket.DispatchMessageQueue();
+            websocket.DispatchMessageQueue();
       #endif
     }
     catch
@@ -44,34 +45,23 @@ public class OtherController : MonoBehaviour
     websocket.OnMessage += (bytes) =>
     {
       string message = System.Text.Encoding.UTF8.GetString(bytes);
+      Patch patch = JsonUtility.FromJson<Patch>(message);
 
-      Location location = JsonUtility.FromJson<Location>(message);
-      Vector2 vector = new Vector2(location.x, location.y);
+      // Ignore our own updates
+      if (patch.uuid != SystemInfo.deviceUniqueIdentifier)
+      {
+        Vector2 vector = new Vector2(patch.x, patch.y);
 
-      // Get the transform component of the player
-      Transform transform = GetComponent<Transform>();
+        // Get the transform component of the player
+        Transform transform = gameObject.GetComponent<Transform>();
 
-      // Update the actual position of the player
-      transform.position = vector;
+        // Update the actual position of the player
+        transform.position = vector;
+      }
     };
-
-    // Keep sending messages at every 0.3s
-    InvokeRepeating("SendWebsocket", 0.0f, 0.3f);
 
     // waiting for messages
     await websocket.Connect();
-  }
-
-  async void SendWebsocket()
-  {
-    if (websocket.State == WebSocketState.Open)
-    {
-      // Sending bytes
-      await websocket.Send(new byte[] { 10, 20, 30 });
-
-      // Sending plain text
-      await websocket.SendText("plain text message");
-    }
   }
 
   private async void OnApplicationQuit()
