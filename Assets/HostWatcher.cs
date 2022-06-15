fdaca31510c99bd0f3d062beb613eb2b8dc1d39c
Sjors van Holst <sjorsvanholst@gmail.com>
2022-06-15 13:32:08 +0200

    ✨ Added multiplayer menu

---
+++ Assets/HostWatcher.cs
@@ -0,0 +1,60 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HostWatcher : MonoBehaviour
+{
+  private TextMeshProUGUI hostShort;
+
+  private GameObject websocketObject;
+  private WebsocketWatcher websocketScript;
+
+  private void Start()
+  {
+    websocketObject = GameObject.Find("WebsocketWatcher");
+    websocketScript = websocketObject.GetComponent<WebsocketWatcher>();
+
+    // Generate a short code and show it too the user
+    hostShort = GameObject.Find("HostText").GetComponent<TextMeshProUGUI>();
+    hostShort.text = GenerateCode(5);
+
+    websocketScript.StartWebsocket();
+    
+    websocketScript.websocket.OnOpen += WebsocketOpen;
+    websocketScript.websocket.OnMessage += WebsocketResponse;
+  }
+
+  private void WebsocketOpen() {
+
+  // Share the short code with the server so someone else can subscribe to it
+          Action action = new Action();
+
+      action.code = hostShort.text;
+      action.action = "hosting";
+
+      websocketScript.SendWebsocket(action);
+  }
+
+  private void WebsocketResponse(byte[] bytes) {
+    // Once a partner has been found we can go to the next scene
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+  }
+
+  private string GenerateCode(int length)
+  {
+		// I've removed characters that are hard to distinguish from each other
+    string randomCharacters = "abcdefghjkmnopqrstuvwxyz023456789";
+    string randomString = "";
+
+    for (int i = 0; i < length; i++)
+    {
+      int randomIndex = UnityEngine.Random.Range(0, randomCharacters.Length);
+      char randomCharacter = randomCharacters[randomIndex];
+
+      randomString += randomCharacter;
+    }
+
+    return randomString;
+  }
+
+}
---
+++ Assets/WebsocketWatcher.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using NativeWebSocket;
+using Newtonsoft.Json;
+
+public class WebsocketWatcher : MonoBehaviour
+{
+    Formatting jsonFormatting;
+    JsonSerializerSettings jsonSettings;
+
+    public WebSocket websocket
+    {
+        get => Globals.websocket;
+    }
+
+    private void Start()
+    {
+        jsonSettings = new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore };
+        jsonFormatting = Formatting.None;
+    }
+
+    private void Update()
+    {
+        if (Globals.websocket != null) {
+            #if !UNITY_WEBGL || UNITY_EDITOR
+                Globals.websocket.DispatchMessageQueue();
+            #endif
+        }
+    }
+
+    async public void StartWebsocket()
+    {   
+        Globals.websocket = new WebSocket("wss://hvg-server.deno.dev/v1/socket");
+
+        Globals.websocket.OnOpen += OpenWebsocket;
+        Globals.websocket.OnError += ErrorWebsocket;
+        Globals.websocket.OnClose += CloseWebsocket;
+
+        await Globals.websocket.Connect();
+    }
+
+    private void OpenWebsocket() {
+        Debug.Log("Connection open!");
+    }
+
+    private void ErrorWebsocket(string error) {
+        Debug.Log("Error: " + error);
+    }
+
+    private void CloseWebsocket(WebSocketCloseCode error) {
+        Debug.Log("Connection closed!");
+    }
+
+    async public void SendWebsocket(object data)
+    {
+        if (Globals.websocket == null) {
+            return;
+        }
+
+        string json = JsonConvert.SerializeObject(data, jsonFormatting, jsonSettings);
+
+        // We can abort the update if the WebSocket is closed 
+        if (Globals.websocket.State != WebSocketState.Open)
+        {
+            await Globals.websocket.Connect();
+        }
+        
+        await Globals.websocket.SendText(json);
+    }
+
+    private async void OnApplicationQuit()
+    {
+        if (Globals.websocket != null)
+        {
+            await Globals.websocket.Close();
+        }
+    }
+}
