c1fb8ed3501ab5e8afd094930f35fd505fc93ab4
Tim van Ingen <timvaningen63@gmail.com>
2022-06-23 02:53:29 +0200

    :whale: made button ui for minigame

---
+++ Assets/BtnClickManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnClickManager : MonoBehaviour
+{
+    public GameObject greenLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        greenLight.SetActive(false);
+    }
+
+    public void BtnClick() {
+        StartCoroutine(GreenToggle());
+    }
+    IEnumerator GreenToggle() {
+        greenLight.SetActive(true);
+        yield return new WaitForSeconds(1);
+        greenLight.SetActive(false);
+    }
+}
